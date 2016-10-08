98b20541f8a77d6e185b2361d25efe677fb854c6
Mabu Manaileng <mabu@itechhub.co.za>
2016-10-08 14:12:07 +0200

    Integration

--- waterly/auth.py
+++
@@ -1,9 +0,0 @@
-class PostgreSQLAuth:
-
-    database = {
-        'db_host':'localhost',
-        'db_pass':'waterly05',
-        'db_port':'5432',
-        'db_name':'dbwaterly',
-        'db_user':'waterly'
-    }
