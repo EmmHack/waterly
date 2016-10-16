0dc9cb230aa161c123b75202805bc7f18eed6e1f
Ofentswe Lebogo <ofentswel@gmail.com>
2016-10-16 08:08:35 -0600

    function that check date difference and then sort after

--- app/static/app/js/d3_visualisations.js
+++ app/static/app/js/d3_visualisations.js
@@ -1,7 +1,6 @@
 $(function() {
     var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
                       "Sep", "Oct", "Nov", "Dec"];
-
     var consumers = null;
     var chosen_customer = null;
     var svg = null;
@@ -9,7 +8,11 @@ $(function() {
     var tooltip = d3.select("body").append("div").attr("class", "toolTip");
     var parseDateTime = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
     var parseTime  = d3.timeParse("%Y-%m-%dT%H:%M:%S-%H:%M");
-
+    var hours_data = null;
+    var days_data = null;
+    var months_data = null;
+    var years_data = null;
+    
     url = 'api/consumers/'
     $.ajax({
         type : 'GET',
@@ -21,8 +24,9 @@ $(function() {
     });
 
     // Get latest data
-    //setInterval(gen_latest_data, 3600000)
-    //gen_latest_data();
+    var hour = 3600000;
+    setInterval(gen_latest_data, hour);
+    gen_latest_data();
     
     function gen_latest_data() {
         url = 'app/generate_plot_data';
@@ -33,30 +37,41 @@ $(function() {
         });
     }
 
-    var hours_data = null;
+    function get_consumer_consumption(consumer) {
+        url = 'api/list_consumption_reading/' + consumer;
+        $.ajax({
+            type : 'GET',
+            url  : url,
+            async: false,
+            success :  function(data){
+                hours_data = data;
+            }
+        });
 
-    url = 'api/list_consumption_reading/' + consumers[0].meter_no;
-    $.ajax({
-        type : 'GET',
-        url  : url,
-        async: false,
-        success :  function(data){
-            hours_data = data;
-        }
-    });
+        days_data = retrieve_data('day');
+        months_data = retrieve_data('month');
+        years_data = retrieve_data('year');
+    }
+
+    get_consumer_consumption(consumers[0].meter_no);
+    visualise();
     
-    function get_hourly(){
-        var days_data = retrieve_data('day');
-        var months_data = retrieve_data('month');
-        var years_data = retrieve_data('year');
 
+    $("#add-user").on('click', 'li', function () {
+        chosen_customer = $(this.id).selector;
+
+        get_consumer_consumption(parseInt(chosen_customer));
+        visualise();
+        
+    });
+
+    function visualise() {
         if (years_data.length >= 3) {
             plot_data(hours_data, 0, '#interactive');
             bar_plot(months_data);
         } else if (months_data.length >= 6) {
             plot_data(months_data, 0, '#interactive');
             bar_plot(months_data);
-            //console.log(months_data)
         } else if (days_data.length >= 15) {
             plot_data(days_data, 0, '#interactive');
             bar_plot(months_data);
@@ -66,8 +81,6 @@ $(function() {
         }
     }
 
-    get_hourly();
-
     /**
     *   function that retrive data fro a choosen date time range
     *   @param level => as day or month or hours or weeks
@@ -75,7 +88,9 @@ $(function() {
     */
     function retrieve_data(level) {
         var groupdata = _.groupBy(hours_data, function(data) {
-            return moment(data.date).startOf(level).format();
+            var date = parseDateTime(data.date);
+            date.setHours(date.getHours() + 2);
+            return moment(date.toISOString()).startOf(level).format();
         });
 
         var averages = [];
@@ -97,25 +112,8 @@ $(function() {
     }
 
     for(var i = 0; i < consumers.length; i++) {
-        $("#add-user").append("<li>"+consumers[i].meter_no+"</li>");
+        $("#add-user").append("<li id='"+consumers[i].meter_no+"' >"+consumers[i].name+"</li>");
     }
-
-    $("#add-user").on('click', 'li', function () {
-        chosen_customer = $(this).text();
-        var hours_data = null;
-
-        url = 'api/list_consumption_reading/' + chosen_customer;
-        $.ajax({
-            type : 'GET',
-            url  : url,
-            async: false,
-            success :  function(data){
-                hours_data = data;
-            }
-        });
-
-        get_hourly();
-    });
 
     /**
     *   function that manipulate the date  
@@ -125,6 +123,16 @@ $(function() {
     function getDate(d) {
         var date = new Date(d);
         return monthNames[date.getMonth()];
+    }
+
+    /**
+    *   function that sorts dates
+    *   @param a => date 1 
+    *   @param b => date 2
+    *   @return the difference
+    */
+    function sortByDateAscending(a, b) {
+            return a.date - b.date;
     }
 
     /**
@@ -141,7 +149,7 @@ $(function() {
         var y = d3.scaleLinear().range([height, 0]);
 
         x.domain(data.map(function(d) { return getDate(d.date); }));
-        y.domain([0, d3.max(data, function(d) { return d.reading; })]);
+        y.domain([d3.max(data, function(d) { return d.reading; })-10, d3.max(data, function(d) { return d.reading; })]);
         
         if(bar_svg == null) {
             bar_svg = d3.select("#catchart").append("svg")
@@ -162,20 +170,23 @@ $(function() {
                 .attr("class", "bar")
                 .attr("x", function(d) { return x(getDate(d.date)); })
                 .attr("width", x.bandwidth())
+                .attr("text-anchor", "end")
                 .attr("y", function(d) { return y(d.reading); })
                 .attr("height", function(d) { return height - y(d.reading); })
-                .on("mouseover",function(){
-                    tooltip.style("left", d3.event.pageX - 50 + "px")
-                        .style("top", d3.event.pageY - 70 + "px")
+                .on("mousemove",function(d){
+                    tooltip.style("left", (d3.event.pageX - 50) + "px")
+                        .style("top", (d3.event.pageY - 70) + "px")
                         .style("display", "inline-block")
-                        .html("<hr/>"+d.date + "<br>" + (d.reading));
+                        .html("<hr/>"+(d.date) + "<br>" + (d.reading));
                 });
                 
             bar_svg.append("g")
+                .attr("class", "x-axis")
                 .attr("transform", "translate(0," + height + ")")
                 .call(d3.axisBottom(x));
 
             bar_svg.append("g")
+                .attr("class", "y-axis")
                 .call(d3.axisLeft(y));
 
         } else {
@@ -201,6 +212,7 @@ $(function() {
     *   @return 
     */
     function plot_data(data, type, id_division) {
+        console.log(data);
         var screen_width = document.querySelector("#interactive").clientWidth, 
             screen_height = document.querySelector("#interactive").clientHeight;
 
@@ -240,8 +252,13 @@ $(function() {
             d.reading = +d.reading;
         });
 
+        console.log(data);
+
+      data = data.sort(sortByDateAscending);
+      console.log(data);
+
         x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain([0, d3.max(data, function(d) { return d.reading; })]);
+        y.domain([d3.min(data, function(d) { return d.reading; })-10, d3.max(data, function(d) { return d.reading; })]);
 
          // DON'T DELETE YOU WILL GET YOURSELF KILLED.
         /*svg.append("path")
@@ -271,7 +288,7 @@ $(function() {
                     tooltip.style("left", d3.event.pageX - 50 + "px")
                         .style("top", d3.event.pageY - 70 + "px")
                         .style("display", "inline-block")
-                        .html("<h5>Details</h5><hr/>" + getDate((d.date)) +
+                        .html("<h5>Details</h5><hr/>" + (d.date) +
                               "<br>" + (d.reading));
                 })
                 .on("mouseout", function(d) {
@@ -285,7 +302,6 @@ $(function() {
                     console.log("#TO DO");
                 })
                 .attr("cx", function(d) {
-                    console.log(d);
                     return x(d.date);
                 })
                 .attr("cy", function(d) {
@@ -299,7 +315,8 @@ $(function() {
 
             svg.append("g")
                 .attr("class","y-axis")
-                .call(d3.axisLeft(y));
+                .call(d3.axisLeft(y)
+                    .ticks(10));
 
             var focus = svg.append("g")
             .attr("class", "focus")
